530a527def44d3ee88893c6db1f3db895bbcd380
Arushi <arushisr24@gmail.com>
2022-06-17 17:05:36 +0530

    Update 1 in the GUI

--- individual_functions/gui.py
+++ individual_functions/gui.py
@@ -1,17 +1,24 @@
 import tkinter
+import tkinter.filedialog
 import customtkinter
 
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
-root_tk = customtkinter.CTk()  # create CTk window like the Tk window
-root_tk.geometry("400x240")
+app = customtkinter.CTk() 
+app.geometry("400x240")
+app.title("Zipload")
 
-def button_function():
-    print("button pressed")
+def clicked():
+    folder = tkinter.filedialog.askdirectory()
+    #ziploadThese(folder)
+    print(folder)
+
+app.welcome_label= customtkinter.CTkLabel(text="         Welcome to Zipload!",text_font=("Roboto Medium", -24))
+app.welcome_label.grid(row=0, column=0, pady=20, padx=20, sticky="nwe")
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root_tk, command=button_function)
+button = customtkinter.CTkButton(master=app, command=clicked)
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-root_tk.mainloop()
+app.mainloop()
