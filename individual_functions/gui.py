ffb475bf02fed1b2256a7bf8b3348174b4c06b4f
Arushi <arushisr24@gmail.com>
2022-06-17 19:14:44 +0530

    Final GUI

--- individual_functions/gui.py
+++ individual_functions/gui.py
@@ -14,11 +14,14 @@ def clicked():
     #ziploadThese(folder)
     print(folder)
 
-app.welcome_label= customtkinter.CTkLabel(text="         Welcome to Zipload!",text_font=("Roboto Medium", -24))
+app.welcome_label= customtkinter.CTkLabel(text="          Welcome to Zipload",text_font=("Roboto Medium", -24))
 app.welcome_label.grid(row=0, column=0, pady=20, padx=20, sticky="nwe")
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=app, command=clicked)
+button = customtkinter.CTkButton(master=app, text = "Choose the folder to be Ziploaded", command=clicked)
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+app.welcome_label= customtkinter.CTkLabel(text="                      a python tool by arushi",text_font=("Roboto Medium", -12))
+app.welcome_label.grid(row=10, pady=130, column=0, sticky="nwe")
 
 app.mainloop()
